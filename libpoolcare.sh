bfe45ba9dded74948fc689f55e1c31b34ade3309
Mikhail Kshevetskiy <kl@slind.org>
2007-06-22 13:44:40 +0400

    [PATCH] preliminary support for architectural corner cases
    This patch implements preliminary support for architectural corner
    which opens distro maintainers/release managers the following
    possibilities:
    * Ability to override source version/section on per-architectural
      basis inside suite.
    * Ability to provide singled-out entities in database, while keeping
      common entities for all other arches.
    
    Details:
     - change of sql database structure (arch field was added)
     - incapsulate sql query to separate functions
     - get_deb_poolpath() behavior was changed
     - obsolete override_poolpath() use override_get_pkg_suites_list() instead
     - cleanup
    
    Signed-off-by: Mikhail Kshevetskiy <kl@slind.org>
    Signed-off-by: Alexander Shishkin <virtuoso@slind.org>

--- libpoolcare.sh
+++ libpoolcare.sh
@@ -16,13 +16,8 @@ mkoverrides() {
 	$SQLCMD "create table overrides (
 		pkgname varchar NOT NULL,
 		version varchar NOT NULL,
-		suite char(24),
-		component varchar NOT NULL);"
-	$SQLCMD "create table arch_overrides (
-		pkgname varchar NOT NULL,
-		version varchar NOT NULL,
-		arch NOT NULL,
-		suite char(24),
+		suite char(24) NOT NULL,
+		arch char(32) NOT NULL,
 		component varchar NOT NULL);"
 }
 
@@ -35,15 +30,15 @@ yell() {
 
 translate_section() {
 	local _comp="$1"
-	local _pkgname="$2"
+	local _source="$2"
 	case "$_comp" in
 		devel|libdevel)
 			echo "host-tools"
-			yell "WARNING: section $_comp in package $_pkgname"
+			yell "WARNING: section $_comp in package $_source"
 			;;
 		libs)
 			echo "core"
-			yell "WARNING: section $_comp in package $_pkgname"
+			yell "WARNING: section $_comp in package $_source"
 			;;
 		*)
 			echo "$_comp"
@@ -52,19 +47,21 @@ translate_section() {
 }
 
 # get most recent package version from overrides.db
-# $1 -- binary package name
-# $2 -- package suite name
+# $1 -- source package name
+# $2 -- package suite name (optional)
+# $3 -- arch name (optional)
 override_get_pkg_version() {
-	local _pkgname="$1"
+	local _source="$1"
 	local _suite="$2"
+	local _arch="$3"
 
-	local _condition="pkgname='$_pkgname'"
+	local _condition="pkgname='$_source' and arch='$_arch'"
 	if [ -n "$_suite" ]; then
 		_condition="$_condition and suite='$_suite'"
 	fi
-	
-	local _version=`$SQLCMD "select version from overrides
-		where $_condition order by version desc limit 1"`
+
+	local _version=`$SQLCMD "SELECT version FROM overrides
+		WHERE $_condition ORDER BY version DESC LIMIT 1"`
 	if [ -z "$_version" ]; then
 		echo "Can't find $_pkgname version in overrides.db" >&2
 		exit 1
@@ -73,80 +70,50 @@ override_get_pkg_version() {
 	echo "$_version"
 }
 
+# get a list of suites for .deb package from overrides.db
+# $1 -- source package name of .deb file
+# $2 -- package version
+# $3 -- arch name (optional)
+# returns a list of suites
+override_get_pkg_suites_list() {
+	local _source="$1"
+	local _version="$2"
+	local _arch="$3"
+
+	$SQLCMD "SELECT DISTINCT suite FROM overrides
+			WHERE pkgname='$_source'
+			AND version='$_version'
+			AND (arch='$_arch' OR arch='')
+			ORDER BY suite DESC"
+}
+
+#get a list of components for .deb package from overrides.db
+# $1 -- source package name of .deb file
+# $2 -- package version
+# $3 -- arch name (optional)
+override_get_pkg_componets_list() {
+	local _source="$1"
+	local _version="$2"
+	local _arch="$3"
+
+	$SQLCMD "SELECT DISTINCT component FROM overrides
+			WHERE pkgname='$_source'
+			AND version='$_version'
+			AND (arch='$_arch' OR arch='')
+			ORDER BY component DESC"
+}
+
 # match source package against overrides db
-# $1 -- source package name
-# $2 -- source package version
-# returns 'suite/component' path component
-override_poolpath() {
-	local _pkgname="$1"
+# $1 -- source package name of
+# $2 -- package version
+# returns a list of 'suite/component' path component
+override_get_src_poolpath_list() {
+	local _source="$1"
 	local _pkgver="$2"
 
-	# 1. Check if this package/version record exists in db
-	_overrid=`$SQLCMD "select suite from overrides
-			    where pkgname='$_pkgname'
-			      and version='$_pkgver'"`
-
-	if [ -z "$_overrid" ]; then
-		# -- it doesn't
-		# 2.A. Check if some other version of the package is in
-		# the current $DEVSUITE in db.
-		_overrid=`$SQLCMD "select version from overrides
-				    where pkgname='$_pkgname'
-				      and suite='$DEVSUITE'"`
-		if [ -n "$_overrid" ]; then
-			# -- it is
-			# 3.A.A. Compare our version with the one in the db
-			yell "Warning: there is another version of " -n
-			yell "$_pkgname ($_overrid) in $DEVSUITE."
-
-			dpkg --compare-versions "$_pkgver" lt "$_overrid"
-			if [ "$?" != "0" ]; then
-				# -- our version is newer
-				# 4.A.A.A. Put our version to $DEVSUITE and
-				# move the older one to attic.
-				yell "...but $_pkgver is newer, so moving " -n
-				yell "$_overrid to attic"
-
-				_comp=`$SQLCMD "select component from overrides
-						 where pkgname='$_pkgname'"`
-				$SQLCMD "update overrides
-					    set version='$_pkgver'
-					  where pkgname='$_pkgname'
-					    and suite='$DEVSUITE';
-					 insert into overrides
-					 values ('$_pkgname', '$_overrid',
-						 'attic', '$_comp')"
-				# That is to say, our package/version is in
-				# $DEVSUITE.
-				_suite="$DEVSUITE"
-			else 
-				# -- our version is older than the one in db
-				# 4.A.A.B. Put ourselves to 'attic' right away.
-				_suite="attic"
-			fi
-		else
-			# -- it is not: the package is new
-			# 3.A.B. Add package/version to $DEVSUITE.
-			yell "# adding $_pkgname=$_pkgver to overrides"
-			$SQLCMD "insert into overrides values('$_pkgname', '$_pkgver', '$DEVSUITE', 'host-tools')"
-			_suite="$DEVSUITE"
-		fi
-	else
-		# -- it is
-		# 2.B. Do nothing.
-		yell "# $_pkgname=$_pkgver already in overrides"
-		_suite="$_overrid"
-	fi
-
-	# Find the component
-	_comp=`$SQLCMD "select component from overrides
-			 where suite='$_suite'
-			   and pkgname='$_pkgname'"`
-	if [ -z "$_comp" ]; then
-		_comp="host-tools"
-	fi
-
-	echo "$_suite/$_comp"
+	$SQLCMD "SELECT DISTINCT suite || '/' || component FROM overrides
+			WHERE pkgname='$_source'
+			AND version='$_pkgver'"
 }
 
 # Produce a 'Sources' entry from .dsc.
@@ -155,10 +122,10 @@ override_poolpath() {
 dsc_to_Sources() {
 	local _dscfile="$1"
 	local _sourcespath="$2"
-	_dscfilename="`echo -n $_dscfile | sed -e 's,^.*/,,'`"
-	_dscmd5="`md5sum $_dscfile | cut -d' ' -f1`"
-	_dscsz="`stat -c %s $_dscfile`"
-	_dscdir="`echo -n $_dscfile | sed -e 's,/[^/]*$,,' -e 's,^.*pool/,pool/,'`"
+	local _dscfilename="`echo -n $_dscfile | sed -e 's,^.*/,,'`"
+	local _dscmd5="`md5sum $_dscfile | cut -d' ' -f1`"
+	local _dscsz="`stat -c %s $_dscfile`"
+	local _dscdir="`echo -n $_dscfile | sed -e 's,/[^/]*$,,' -e 's,^.*pool/,pool/,'`"
 
 	gawk                               \
 		-v dscmd5="$_dscmd5"       \
@@ -192,7 +159,7 @@ get_deb_header() {
 	local _debfile="$1"
 	local _hdr="$2"
 
-	_value=`ar p $_debfile control.tar.gz | tar zxO ./control | \
+	local _value=`ar p $_debfile control.tar.gz | tar zxO ./control | \
 		grep "^${_hdr}: " | cut -d' ' -f2`
 	if [ "$_hdr" = "Version" ]; then
 		_value=`echo $_value | sed -e 's,^[^:]*:,,'`
@@ -206,67 +173,70 @@ get_deb_header() {
 # $1 -- path to .deb file
 get_deb_distpath() {
 	local _debfile="$1"
+	local _comp
+	local _path
 
-	_source="`get_deb_header $_debfile Source`"
+	local _version="`get_deb_header $_debfile Version`"
+	local _arch="`get_deb_header $_debfile Architecture`"
+	local _source="`get_deb_header $_debfile Source`"
 	if [ -z "$_source" ]; then
 		_source="`get_deb_header $_debfile Package`"
 	fi
 
-	_comp="`get_deb_header $_debfile Section`"
-	if [ -z "$_comp" ]; then
-		yell "WARNING: package $_debfile lacks a " -n
-		yell "'Section:' header, assuming 'core'"
-		_comp=core
-	fi
-
-	_version="`get_deb_header $_debfile Version`"
-	_arch="`get_deb_header $_debfile Architecture`"
-	_suite=`$SQLCMD "select suite from overrides
-			  where pkgname='$_source'
-			    and version='$_version'"`
-	if [ -z "$_suite" ]; then
+	local _path_list=`override_get_src_poolpath_list $_source $_version`
+	if [ -z "$_path_list" ]; then
 		yell "WARNING: cannot find $_source=$_version in" -n
-		yell "overrides table, assuming $DEVSUITE"
-		_suite="$DEVSUITE"
-	fi
-	#yell "#### $_debfile: [$_comp] [$_suite] [$_source] [$_version]"
+		yell "overrides table, assuming suite=$DEVSUITE"
 
-	_comp="`translate_section $_comp $_source`"
-	if [ "$_arch" = "all" ]; then
-		for _arch in $ARCHES; do
-			echo "$_suite/$_comp/binary-$_arch"
-		done
-	else
-		echo "$_suite/$_comp/binary-$_arch"
+		local _comp="`get_deb_header $_debfile Section`"
+		if [ -z "$_comp" ]; then
+			yell "WARNING: package $_debfile lacks a " -n
+			yell "'Section:' header, assuming 'core'"
+			_comp=core
+		else
+			_comp="`translate_section $_comp $_source`"
+		fi
+		_path_list="$DEVSUITE/$_comp"
 	fi
+
+	for _path in $_path_list; do
+		if [ "$_arch" = "all" ]; then
+			for _a in $ARCHES; do
+				echo "$_path/binary-$_a"
+			done
+		else
+			echo "$_path/binary-$_arch"
+		fi
+	done
 }
 
 # determine where a given package belongs in a pool
 # $1 -- path to .deb file
 get_deb_poolpath() {
 	local _debfile="$1"
+	local _comp
 
-	_comp="`get_deb_header $_debfile Section`"
-	if [ -z "$_comp" ]; then
-		yell "WARNING: package $_debfile lacks a " -n
-		yell "'Section:' header, assuming 'core'"
-		_comp=core
-	fi
-
-	_source="`get_deb_header $_debfile Source`"
+	local _version="`get_deb_header $_debfile Version`"
+	local _arch="`get_deb_header $_debfile Architecture`"
+	local _source="`get_deb_header $_debfile Source`"
 	if [ -z "$_source" ]; then
 		_source="`get_deb_header $_debfile Package`"
 	fi
 
-	_comp_overrid=`$SQLCMD "select component from overrides
-				 where pkgname='$_source'
-				   and version='$_version'"`
-	if [ -n "$_comp_overrid" ]; then
-		_comp="$_comp_overrid"
+	local _comp_list=`override_get_pkg_componets_list $_source $_version $_arch`
+	if [ -z "$_comp_list" ]; then
+		_comp_list="`get_deb_header $_debfile Section`"
+		if [ -z "$_comp_list" ]; then
+			yell "WARNING: package $_debfile lacks a " -n
+			yell "'Section:' header, assuming 'core'"
+			_comp_list=core
+		fi
 	fi
 
 	_pkgprefix=`expr "$_source" : "\(lib.\|.\)"`
-	echo "pool/$_comp/$_pkgprefix/$_source"
+	for _comp in $_comp_list; do
+		echo "pool/$_comp/$_pkgprefix/$_source"
+	done
 }
 
 # output package's information (control) to a Packages file
@@ -275,10 +245,10 @@ get_deb_poolpath() {
 deb_to_Packages() {
 	local _debfile="$1"
 	local _packagespath="$2"
-	_debfilename="`echo -n $_debfile | sed -e 's,^.*/,,'`"
-	_debmd5="`md5sum $_debfile | cut -d' ' -f1`"
-	_debsz="`stat -c %s $_debfile`"
-	_debdir="`echo -n $_debfile | sed -e 's,/[^/]*$,,' -e 's,^.*pool/,pool/,'`"
+	local _debfilename="`echo -n $_debfile | sed -e 's,^.*/,,'`"
+	local _debmd5="`md5sum $_debfile | cut -d' ' -f1`"
+	local _debsz="`stat -c %s $_debfile`"
+	local _debdir="`echo -n $_debfile | sed -e 's,/[^/]*$,,' -e 's,^.*pool/,pool/,'`"
 
 	ar p $_debfile control.tar.gz | tar zxO ./control | \
 	gawk                               \

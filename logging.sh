9f739c846b4808e4a2022a3597d0b746edf45c7c
Alexander Shishkin <virtuoso@slind.org>
2007-07-09 13:22:40 +0400

    add logging functionality with a separate library Signed-off-by: Alexander Shishkin <virtuoso@slind.org>

---
+++ logging.sh
@@ -0,0 +1,41 @@
+# logging
+#  * LOGFILE -- full pathname of a log file which will contain the full output
+#               of commands executed
+#  * REPORTFILE -- pathname of a file which will contain log messages
+
+# print a log message to REPORTFILE
+# $1 -- message to be printed
+# $2 -- options to 'echo'
+logmsg()
+{
+	local _msg="$1"
+	local _opt="$2"
+
+	if [ "${_opt#*n}" != "$_opt" ]; then
+		_msg="`date +'%H:%M:%S'` $_msg"
+	fi
+	echo $_opt "$_msg" >> "$REPORTFILE"
+}
+
+# execute a command, log its output to LOGFILE and put results to REPORTFILE
+# $1 -- command to be executed
+# $2 -- (optional) message to REPORTFILE
+logcmd()
+{
+	local _cmd="$1"
+	local _msg="$2"
+
+	if [ -z "$_msg" ]; then
+		_msg="$_cmd"
+	fi
+
+	logmsg "$_msg... " -n
+	( $_cmd 2>&1;                    \
+		if [ "$?" = "0" ]; then  \
+			logmsg "OK";     \
+		else                     \
+			logmsg "FAILED"; \
+		fi                       \
+	) | tee -a "$LOGFILE"
+}
+
